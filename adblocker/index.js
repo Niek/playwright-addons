2ba23716e0d448238e88e76adbc342ab75a0279a
Niek van der Maas <mail@niekvandermaas.nl>
2020-06-24 15:31:15 +0200

    Add API docs, adblocker options

--- adblocker/index.js
+++ adblocker/index.js
@@ -1,11 +1,29 @@
+/**
+ * @module playwright-addons/adblocker
+ */
+
 import blocker from '@cliqz/adblocker-playwright';
 import fetch from 'cross-fetch';
 
-export default async function (br) {
+/**
+ * Enable the ad blocker add-on
+ * @param {Browser} br - the Browser object of Playwright
+ * @param {Object} [options={}] - optional options to pass
+ * @param {string} [options.customList] - provide a custom block list URL instead of the standard one
+ * @param {boolean} [options.blockTrackers=false] - block trackers in addition to ads
+ */
+export default async function (br, options = {}) {
     if (typeof br !== 'object' || !br.contexts) {
         console.error('Need to provide a Playwright Browser object');
     } else {
-        const bl = await blocker.PlaywrightBlocker.fromPrebuiltAdsAndTracking(fetch);
+        if (options.customList) {
+            const bl = await blocker.PlaywrightBlocker.fromLists(fetch, options.customList);
+        } else if (options.blockTrackers) {
+            const bl = await blocker.PlaywrightBlocker.fromPrebuiltAdsAndTracking(fetch);
+        } else {
+            const bl = await blocker.PlaywrightBlocker.fromPrebuiltAdsOnly(fetch);
+        }
+
         br.contexts().forEach(c => {
             // Existing pages
             for (let i = 0; i < c.pages().length; i++) {
